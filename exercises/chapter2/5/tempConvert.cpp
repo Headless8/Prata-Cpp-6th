5659480d1d983950994f46ea36f91b3fbb190ca6
Headless <headlessfirst@gmail.com>
2015-08-09 09:55:00 +0300

    Deleted the tabs in 'tempConvert.cpp'

--- exercises/chapter2/5/tempConvert.cpp
+++ exercises/chapter2/5/tempConvert.cpp
@@ -6,8 +6,8 @@ double CelsiusToFahr(
 	double aCelsius)
 {
 	const double CELSFAHR_RATIO = 1.8;
-	const int 	 SHIFT 			= 32;
-	double 		 fahr;
+	const int SHIFT = 32;
+	double fahr;
 
 	fahr = CELSFAHR_RATIO * aCelsius + SHIFT;
 	return fahr;
